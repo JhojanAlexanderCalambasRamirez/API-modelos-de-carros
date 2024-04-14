ae42aa3094acf72b46486153c8fafec324421701
JhojanAlexanderCalambasRamirez <jhojan.calambas@uao.edu.co>
2024-04-14 18:02:29 -0500

    La API ya me devuelve los valores que le pido en una barra de buscar :D

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
-import React from 'react';
-import './App.css';
-import CarList from './components/CarList';
+import React, { useState } from 'react';
 
 function App() {
+  const [carModel, setCarModel] = useState('');
+  const [carData, setCarData] = useState([]);
+
+  const handleChange = (event) => {
+    setCarModel(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    fetch(`https://api.api-ninjas.com/v1/cars?model=${carModel}`, {
+      method: 'GET',
+      headers: {
+        'X-Api-Key': 'pgSMtTlugtFy1S6JvjAVdQ==o7ve1mMiFLVaMxPL'
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      setCarData(data);
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+    });
+  };
+
   return (
-    <div className="App">
-      <h1>Mi Aplicación de Búsqueda de Autos</h1>
-      <CarList />
+    <div>
+      <h1>Buscar Modelos de Carro</h1>
+      <input type="text" value={carModel} onChange={handleChange} />
+      <button onClick={handleSubmit}>Enviar Petición</button>
+
+      <h2>Resultados:</h2>
+      <ul>
+        {carData.map((car, index) => (
+          <li key={index}>
+            <p>Modelo: {car.model}</p>
+            <p>Fabricante: {car.make}</p>
+            <p>Año: {car.year}</p>
+            <p>Transmisión: {car.transmission}</p>
+            <p>Combustible: {car.fuel_type}</p>
+            <p>Cilindros: {car.cylinders}</p>
+            <p>Tracción: {car.drive}</p>
+            <p>MPG en ciudad: {car.city_mpg}</p>
+            <p>MPG en carretera: {car.highway_mpg}</p>
+            <p>MPG combinado: {car.combination_mpg}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
